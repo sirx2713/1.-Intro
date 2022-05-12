d530c9dfb2ba1e696419dabc4021cc84e5190ed3
Meng To <shadownessguy@gmail.com>
2022-05-12 13:11:42 -0400

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Meng To on 2022-05-12.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animated/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animated
+//
+//  Created by Meng To on 2022-05-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
